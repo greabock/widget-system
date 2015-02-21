fe3eab6b6f2f7f3738fe6106ca42f648500fe167
Роман Сохарев <greabock@gmail.com>
2015-02-21 13:23:57 +0700

    formated

--- src/Widget.php
+++ src/Widget.php
@@ -4,136 +4,136 @@ use Illuminate\Foundation\Application as App;
 
 class Widget {
 
-    /**
-     * classes registered widgets
-     *
-     * @var array
-     */
-    protected $classes = [];
+	/**
+	 * classes registered widgets
+	 *
+	 * @var array
+	 */
+	protected $classes = [];
 
-    /**
-     * Prepared instances of widgets
-     *
-     * @var array
-     */
-    protected $instances = [];
+	/**
+	 * Prepared instances of widgets
+	 *
+	 * @var array
+	 */
+	protected $instances = [];
 
-    /**
-     * Positions for widgets
-     *
-     * @var array
-     */
-    protected $positions = [];
+	/**
+	 * Positions for widgets
+	 *
+	 * @var array
+	 */
+	protected $positions = [];
 
 
-    public function __construct(App $app)
-    {
-    	$this->app = $app;
-    }
+	public function __construct(App $app)
+	{
+		$this->app = $app;
+	}
 
-    /**
-     * @param string $method
-     * @param array  $arguments
-     * @return mixed
-     */
-    public function __call($method, array $arguments)
-    {
-        array_unshift($arguments, $method);
+	/**
+	 * @param string $method
+	 * @param array  $arguments
+	 * @return mixed
+	 */
+	public function __call($method, array $arguments)
+	{
+		array_unshift($arguments, $method);
 
-        return call_user_func_array([$this, 'show'], $arguments);
-    }
+		return call_user_func_array([$this, 'show'], $arguments);
+	}
 
-    /**
-     * @param  string $class
-     * @param  string $name
-     * @return void
-     */
-    public function register($class, $name, $position = null, $order = null)
-    {
-        $this->classes[$name] = $class;
+	/**
+	 * @param  string $class
+	 * @param  string $name
+	 * @return void
+	 */
+	public function register($class, $name, $position = null, $order = null)
+	{
+		$this->classes[$name] = $class;
 
-        if($position)
-        {
-        	$this->positions[$position][] = compact('name', 'order');
-        }
+		if ($position)
+		{
+			$this->positions[$position][] = compact('name', 'order');
+		}
 
-    }
+	}
 
-    /**
-     * @param  string $name
-     * @return mixed|null
-     */
-    public function show($name)
-    {
+	/**
+	 * @param  string $name
+	 * @return mixed|null
+	 */
+	public function show($name)
+	{
 
-        if ( ! array_key_exists($name, $this->classes)) return null;
+		if ( ! array_key_exists($name, $this->classes)) return null;
 
-        if ( ! array_key_exists($name, $this->instances))
-        {
-            $widget = $this->app->make($this->classes[$name]);
+		if ( ! array_key_exists($name, $this->instances))
+		{
+			$widget = $this->app->make($this->classes[$name]);
 
-            $this->addInstance($widget, $name);
-        }
+			$this->addInstance($widget, $name);
+		}
 
-        return call_user_func_array([$this->instances[$name], 'render'], array_slice(func_get_args(), 1));
-    }
+		return call_user_func_array([$this->instances[$name], 'render'], array_slice(func_get_args(), 1));
+	}
 
-    
-    public function position($position)
-    {
-    	if ( ! array_key_exists($position, $this->positions)) return null;
 
-    	usort($this->positions[$position] , function($a, $b)
-    	{
-    		return ($a['order'] == $b['order']) ? 0 :(( $a['order'] > $b['order']) ? -1 : 1);
+	public function position($position)
+	{
+		if ( ! array_key_exists($position, $this->positions)) return null;
 
-    	});
+		usort($this->positions[$position], function ($a, $b)
+		{
+			return ($a['order'] == $b['order']) ? 0 : (($a['order'] > $b['order']) ? -1 : 1);
 
-    	$carpet = '';
+		});
+
+		$carpet = '';
 
 		$arguments = array_slice(func_get_args(), 1);
 
 
-    	foreach ($this->positions[$position] as $widget)
-    	{
+		foreach ($this->positions[$position] as $widget)
+		{
 
-		    $parameters = $arguments;
+			$parameters = $arguments;
 
-		    array_unshift($parameters, $widget['name']);
+			array_unshift($parameters, $widget['name']);
 
-    		$carpet .= call_user_func_array([$this, 'show'], $parameters);
-    	}
+			$carpet .= call_user_func_array([$this, 'show'], $parameters);
+		}
 
-    	return $carpet;
-    }
-
-
-    public function exists($name)
-    {
-    	if (array_key_exists($name, $this->classes)) return true;
-
-    	return false;
-    }
+		return $carpet;
+	}
 
 
-    public function isEmptyPosition($position)
-    {
-		if ( ! array_key_exists($position, $this->positions)) return true;
-		
-		if ( ! count($this->positions[$position]) ) return true;
+	public function exists($name)
+	{
+		if (array_key_exists($name, $this->classes)) return true;
 
 		return false;
-    }
+	}
 
-    /**
-     * @param $widget
-     * @param string     $name
-     * @return void
-     */
-    protected function addInstance($widget, $name)
-    {
-        $this->instances[$name] = $widget;
-    }
 
+	public function isEmptyPosition($position)
+	{
+		if ( ! array_key_exists($position, $this->positions)) return true;
+
+		if ( ! count($this->positions[$position])) return true;
+
+		return false;
+	}
+
+	/**
+	 * @param        $widget
+	 * @param string $name
+	 * @return void
+	 */
+	protected function addInstance($widget, $name)
+	{
+		$this->instances[$name] = $widget;
+	}
 
 }
+
